f935d7db203cf64d9fe856772b470d649a33edba
Sergei Klokov <s2584835@ya.ru>
2024-01-16 14:48:27 +0300

    feat: solve homework04:task02

---
+++ Task02/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+
+//блок с методами
+void ShowArr(int[] nums) //показывает массив целых чисел
+{
+    Console.Write($"\nМассив:\n[ " + string.Join(", ", nums) + " ]\n");
+}
+
+int[] FillArr(int size)
+{
+    int[] nums = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] = rnd.Next(100,1000);
+    }
+    return nums;
+}
+
+int CountEvenNumbers(int[] nums)
+{
+    int count = 0;
+    foreach (int s in nums)
+    {
+        count += (s % 2 == 0)? 1 : 0;
+    }
+    return count;
+}
+
+//основной блок Main
+Console.Write("Укажите размерность массива: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArr(sizeArr);
+ShowArr(array);
+Console.WriteLine($"В массиве из {sizeArr} элементов кол-во чётных чисел равно {CountEvenNumbers(array)}.");
