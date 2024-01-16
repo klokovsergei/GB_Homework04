f08de147b7636084c0caa67cf87b46ff456d06c6
Sergei Klokov <s2584835@ya.ru>
2024-01-16 16:13:12 +0300

    feat: solve homework04:task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,3 +1,41 @@
 ﻿// Напишите программу, которая перевернёт одномерный массив
 // (первый элемент станет последним, второй – предпоследним и т.д.)
 
+//методы
+int[] CreateArr(int size) //создание и наполнение массива случайными числами
+{
+    int[] nums = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] = rnd.Next(10, 100);
+    }
+    return nums;
+}
+
+void ReversArr(int[] nums)
+{
+    int changeNumber;
+    for (int i = 0; i < nums.Length / 2; i++)
+    {
+        changeNumber = nums[i];
+        nums[i] = nums[nums.Length - 1 - i];
+        nums[nums.Length - 1 - i] = changeNumber;
+    }
+}
+
+void ShowArr(int[] nums) // метод для вывода массива в консоль
+{
+    Console.WriteLine($"Массив: [ {string.Join(", ", nums)} ]");
+}
+
+//основная часть - метод Main
+Console.Write("Укажите размер одномерного целочисленного массива: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArr(sizeArr);
+ShowArr(array);
+
+Console.WriteLine("Вызываю метод ReversArr.");
+ReversArr(array);
+ShowArr(array);
